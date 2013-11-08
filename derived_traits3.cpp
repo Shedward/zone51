6290c7e4d93919dbfc2d6531c786b0aed4ed9429
Maltsev Vlad <shedwardx@gmail.com>
2013-11-08 19:39:10 +0700

    Realisation according parent [Sat00 : 59]

--- derived_traits3.cpp
+++ derived_traits3.cpp
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 template <typename D, typename B>
 class IsDerivedFrom {
 	class Yes {};
@@ -17,18 +19,65 @@ public:
 	}
 };
 
-class X {};
+class X {
+public:
+	void test() {
+		printf("X::test()\n");
+	}
+};
+
+class Y {
+public:
+	void echo() {
+		printf("Y::echo()\n");
+	};
+};
 
 template <typename T>
 class WithChecking : IsDerivedFrom<T,X>{
 
 };
 
+// Default implementation.
+template <typename T, int>
+class PolyImpl {
+public:
+	void test() {
+		T val;
+		val.test();
+	}
+};
+
+// Implementation for Y's with echo instead test
+template <typename T>
+class PolyImpl<T,1> {
+public:
+	void test() {
+		T val;
+		val.echo();
+	}
+};
+
+template <typename T>
+class Poly {
+private:
+	PolyImpl<T, IsDerivedFrom<T, Y>::Is > impl_;
+public:
+	void test() {
+		impl_.test();
+	}
+};
+
 class DX : public X {};
-class Y {};
+class DY : public Y {};
 
 int main() {
 	WithChecking<DX> x;
-	//WithChecking<Y> y;
+	// WithChecking<DY> y;
+
+	Poly<DX> l;
+	l.test();
+	Poly<DY> m;
+	m.test();
 	return 0;
 }
