8fef3d003b34b35273705ed7a2208b4e58912d44
Maltsev Vlad <shedwardx@gmail.com>
2013-11-12 23:45:34 +0700

    Moving square

--- opengl-ex/01/main.cpp
+++ opengl-ex/01/main.cpp
@@ -5,8 +5,15 @@
 
 #include <GL/glut.h>
 
-GLBatch triangleBatch;
+GLBatch squareBatch;
 GLShaderManager shaderManager;
+
+GLfloat vVerts[] = {
+    -0.5f, -0.5f, 0.0f,
+    -0.5f,  0.5f, 0.0f,
+     0.5f,  0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f
+};
 
 void ChangeSize(int w, int h)
 {
@@ -19,14 +26,10 @@ void SetupRC()
     glClearColor(0.0f, 0.0f, 1.0f, 1.0f );
     shaderManager.InitializeStockShaders();
     // Load up a triangle
-    GLfloat vVerts[] = {
-        -0.5f, 0.0f, 0.0f,
-        0.5f, 0.0f, 0.0f,
-        0.0f, 0.5f, 0.0f
-    };
-    triangleBatch.Begin(GL_TRIANGLES, 3);
-    triangleBatch.CopyVertexData3f(vVerts);
-    triangleBatch.End();
+
+    squareBatch.Begin(GL_POLYGON, 4);
+    squareBatch.CopyVertexData3f(vVerts);
+    squareBatch.End();
 }
 
 void RenderScene(void){
@@ -34,9 +37,49 @@ void RenderScene(void){
 
     GLfloat vRed[] = {1.0f, 0.0f, 0.0f, 1.0f};
     shaderManager.UseStockShader(GLT_SHADER_IDENTITY, vRed);
-    triangleBatch.Draw();
+    squareBatch.Draw();
 
     glutSwapBuffers();
+}
+
+GLfloat blockSize = 0.1;
+
+void SpecialKeys(int key, int x, int y){
+    GLfloat stepSize = 0.025f;
+
+    GLfloat blockX = vVerts[0];
+    GLfloat blockY = vVerts[7];
+
+    if (key == GLUT_KEY_UP)
+        blockY += stepSize;
+    if (key == GLUT_KEY_DOWN)
+        blockY -= stepSize;
+    if (key == GLUT_KEY_LEFT)
+        blockX -= stepSize;
+    if (key == GLUT_KEY_RIGHT)
+        blockX += stepSize;
+
+    if (blockX < -1.0f) blockX = -1.0f;
+    if (blockX > (1.0f - blockSize * 2)) blockX = 1.0f - blockSize * 2;
+    if (blockY < -1.0f) blockY = -1.0f;
+    if (blockY > (1.0f - blockSize * 2)) blockY = 1.0f - blockSize * 2;
+
+    vVerts[0] = blockX;
+    vVerts[1] = blockY - blockSize * 2;
+
+    vVerts[3] = blockX + blockSize * 2;
+    vVerts[4] = blockY - blockSize * 2;
+
+    vVerts[6] = blockX + blockSize * 2;
+    vVerts[7] = blockY;
+
+    vVerts[9] = blockX;
+    vVerts[10] = blockY;
+
+    squareBatch.Begin(GL_POLYGON, 4);
+    squareBatch.CopyVertexData3f(vVerts);
+    squareBatch.End();
+    glutPostRedisplay();
 }
 
 int main(int argc, char *argv[])
@@ -49,6 +92,7 @@ int main(int argc, char *argv[])
     glutCreateWindow("Triangle");
     glutReshapeFunc(ChangeSize);
     glutDisplayFunc(RenderScene);
+    glutSpecialFunc(SpecialKeys);
 
     GLenum err = glewInit();
     if (GLEW_OK != err) {
