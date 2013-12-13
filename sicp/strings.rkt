7543a203bbd964911d2870515095b10b03b3dd1a
Maltsev Vlad <shedwardx@gmail.com>
2013-12-13 23:59:46 +0700

    2.57 [Sicp p 154]

--- sicp/strings.rkt
+++ sicp/strings.rkt
@@ -7,3 +7,17 @@
 
 (memq 'a '(b c d))
 (memq 'a '(a (b c d) a))
+
+(define (tst one . res)
+	(write one)
+	(newline)
+	(write res))
+
+
+(define (append-anyway seq val)
+        (append seq (if (list? val) val (list val))))
+
+(define (make-op-list op . seq)
+	(reverse (foldl append (list op) seq)))
+
+(make-op-list 'x '(1 2 5) '(5 6 7) 12)
--- sicp/symbol-deriving.rkt
+++ sicp/symbol-deriving.rkt
@@ -1,21 +1,22 @@
+#lang scheme
 
 (define (deriv exp var)
 	(cond ((number? exp) 0)
 		  ((variable? exp)
 		  	(if (same-var? exp var) 1 0))
-		  ((sum? exp)
-		  	(make-sum (deriv (addend exp) var)
-		  		      (deriv (augend exp) var)))
-		  ((product? exp)
+		  ((is-op? '+ exp)
+		  	(make-sum (deriv (lhs exp) var)
+		  		      (deriv (rhs exp) var)))
+		  ((is-op? '* exp)
 		  	(make-sum
-		  		(make-product (multiplier exp)
-		  					  (deriv (multiplicand exp) var))
-		  		(make-product (deriv (multiplier exp) var)
-		  					  (multiplicand exp))))
-		  ((power? exp)
-		  	(make-product (power exp)
-		  		          (make-power (powered exp) 
-		  		          	          (make-sum (power exp) -1))))
+		  		(make-prod (lhs exp)
+		  					  (deriv (rhs exp) var))
+		  		(make-prod (deriv (lhs exp) var)
+		  					  (rhs exp))))
+		  ((is-op? '^ exp)
+		  	(make-prod (exponent exp)
+		  		          (make-power (variable exp) 
+		  		          	          (make-sum (exponent exp) -1))))
 		  (else
 		  	(error "unexpected expression type -- DERIV" exp))))
 
@@ -25,54 +26,55 @@
 (define (same-var? v1 v2)
 	(and (variable? v1) (variable? v2) (eq? v1 v2)))
 
-(define (=num? x val)
-	(and (number? x) (= x val)))
+(define (make-op-list op . seq)
+	(reverse (foldl append (list op) seq)))
+
+(define (is-op? op exp)
+	(and (pair? exp) (eq? (car exp) op)))
+
+(define (lhs exp) (cadr exp))
+
+(define (rhs exp) 
+	(if (= (length exp) 3)
+		(caddr exp)
+		(make-op-list (car exp) (cddr exp))))
 
 
 
-(define (make-sum a b)
-	(cond ((=num? a 0) b)
-		  ((=num? b 0) a)
-		  ((and (number? a) (number? b)) (+ a b))
-		  (else (list '+ a b))))
+(define (make-sum . seq)
+	(let ((numsum (foldl + 0 (filter number? seq)))
+		  (vals (filter (lambda (x) (not (number? x))) seq)))
+	(cond ((= numsum 0) (if (> (length vals) 1) (make-op-list '+ vals) (car vals)))
+		  ((null? vals) numsum)
+		  (else (make-op-list '+ (list numsum) vals)))))
 
-(define (sum? x)
-	(and (pair? x) (eq? (car x) '+)))
-(define (addend s) (cadr s))
-(define (augend s) (caddr s))
-
-
-
-(define (make-product a b) 
-	(cond ((or (=num? a 0) (=num? b 0)) 0)
-		  ((=num? a 1) b)
-		  ((=num? b 1) a)
-		  ((and (number? a) (number? b)) (* a b))
-		  (else (list '* a b))))
-
-(define (product? x)
-	(and (pair? x) (eq? (car x) '*)))
-(define (multiplier x) (cadr x))
-(define (multiplicand x) (caddr x))
-
-
+(define (make-prod . seq)
+	(let ((numprod (foldl * 1 (filter number? seq)))
+		  (vals (filter (lambda (x) (not (number? x))) seq)))
+	(cond ((= numprod 0) 0)
+		  ((= numprod 1) (make-op-list '* vals))
+		  ((null? vals) numprod)
+		  (else (make-op-list '* (list numprod) vals)))))
 
 (define (make-power a pow)
+	(define (=num? x val)
+		(and (number? x) (= x val)))
 	(cond ((=num? pow 0) 1)
 		  ((=num? pow 1) a)
 		  ((and (number? a) (number? pow)) (expt a pow))
-		  (else (list '** a pow))))
+		  (else (list '^ a pow))))
 
-(define (power? x)
-	(and (pair? x) (eq? (car x) '**)))
-(define (powered x)
-	(cadr x))
-(define (power x)
-	(caddr x))
+(define (exponent exp) (caddr exp))
+(define (variable exp) (cadr exp))
+
+(make-prod 0 'z 5 3)
+(make-prod 1 2 3 (make-sum 'y 9 'x -9))
+(rhs (make-prod 1 2 3 'x))
 
 (deriv '(+ x 3) 'x)
 (deriv '(* x y) 'x)
 
 (deriv '(* (* x y) (+ x 3)) 'x)
 
-(deriv '(+ (* 5 (** x 3)) (+ (* 12 (** x 2)) (+ (* 3 x) 12))) 'x)
+(deriv '(+ (* 5 (^ x 3)) (+ (* 12 (^ x 2)) (+ (* 3 x) 12))) 'x)
+(deriv '(+ (* 5 (^ x 3)) (* 12 (^ x 2)) (* 3 x) 12) 'x)
