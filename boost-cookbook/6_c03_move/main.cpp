b2dcad9ef00ca97006f25edfe474aa588b82288f
Vlad Maltsev <shedwardx@gmail.com>
2014-03-28 20:07:03 +0700

    boost::move [Boost00 p38]

---
+++ boost-cookbook/6_c03_move/main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+#include <boost/swap.hpp>
+#include <boost/move/move.hpp>
+
+#include <assert.h>
+
+namespace other {
+class characteristics {};
+}
+
+struct person_info {
+    bool is_male_;
+    std::string name_;
+    std::string second_name_;
+    other::characteristics charact_;
+
+    void swap(person_info& rhs) {
+        std::swap(is_male_, rhs.is_male_);
+        name_.swap(rhs.name_);
+        second_name_.swap(rhs.second_name_);
+        boost::swap(charact_, rhs.charact_);
+    }
+
+    person_info()
+    {}
+
+    person_info(const person_info& p)
+        : is_male_(p.is_male_),
+          name_(p.name_),
+          second_name_(p.second_name_),
+          charact_(p.charact_)
+    {}
+
+    person_info(BOOST_RV_REF(person_info) person) {
+        swap(person);
+    }
+
+    person_info& operator =(BOOST_COPY_ASSIGN_REF(person_info) person) {
+        if (this != &person) {
+            person_info tmp(person);
+            swap(tmp);
+        }
+        return *this;
+    }
+
+    person_info& operator =(BOOST_RV_REF(person_info) person) {
+        if (this != &person) {
+            swap(person);
+            person_info tmp;
+            tmp.swap(person);
+        }
+        return *this;
+    }
+
+private:
+    BOOST_COPYABLE_AND_MOVABLE(person_info)
+};
+
+int main(int argc, char* argv[]) {
+    person_info vasya;
+    vasya.name_ = "Vasya";
+    vasya.second_name_ = "Snow";
+    vasya.is_male_ = true;
+
+    person_info new_vasya(boost::move(vasya));
+    assert(new_vasya.name_ == "Vasya");
+    assert(new_vasya.second_name_ == "Snow");
+    assert(vasya.name_.empty());
+    assert(vasya.second_name_.empty());
+
+    vasya = boost::move(new_vasya);
+    assert(vasya.name_ == "Vasya");
+    assert(vasya.second_name_ == "Snow");
+    assert(new_vasya.name_.empty());
+    assert(new_vasya.second_name_.empty());
+}
+
