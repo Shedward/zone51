bcac78364fdd5910921cc7fac27417e3c1be5b1d
Vlad Maltsev <shedwardx@gmail.com>
2014-03-31 18:37:42 +0700

    boost::asio::deadline_time [Boost00 p155]

--- boost-cookbook/10_threads/task-processing.cpp
+++ boost-cookbook/10_threads/task-processing.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
 #include <boost/asio/io_service.hpp>
+#include <boost/asio/deadline_timer.hpp>
 #include <boost/thread/thread.hpp>
+#include <boost/system/error_code.hpp>
+#include <boost/make_shared.hpp>
 
 #include <assert.h>
 
@@ -37,6 +40,43 @@ task_wrapped<T> make_task_wrapped(const T& task_unwrapped) {
     return task_wrapped<T>(task_unwrapped);
 }
 
+template <class Func>
+struct timer_task : public task_wrapped<Func> {
+private:
+    typedef task_wrapped<Func> base_t;
+    boost::shared_ptr<boost::asio::deadline_timer> timer_;
+
+public:
+    template <class Time>
+    explicit timer_task(
+            boost::asio::io_service& ios,
+            const Time& duration_or_time,
+            const Func& task_unwrapped)
+        : base_t(task_unwrapped),
+          timer_(boost::make_shared<boost::asio::deadline_timer>(
+                     boost::ref(ios), duration_or_time))
+    {}
+
+    void push_task() const {
+        timer_->async_wait(*this);
+    }
+
+    void operator ()(const boost::system::system_error& err) const {
+        if (err.code()) {
+            base_t::operator ()();
+        } else {
+            std::cerr << err.what() << '\n';
+        }
+    }
+};
+
+template <class Time, class Func>
+inline timer_task<Func> make_timer_task(
+        boost::asio::io_service& ios,
+        const Time& duration_or_time,
+        const Func& task_unwrapped) {
+    return timer_task<Func>(ios, duration_or_time, task_unwrapped);
+}
 } //  namespace detail
 
 class tasks_processor : private boost::noncopyable {
@@ -66,6 +106,18 @@ public:
     void stop() {
         ios_.stop();
     }
+
+    typedef boost::asio::deadline_timer::time_type time_type;
+    template <typename Func>
+    void run_at(time_type time, const Func& f) {
+        detail::make_timer_task(ios_, time, f).push_task();
+    }
+
+    typedef boost::asio::deadline_timer::duration_type duration_type;
+    template <typename Func>
+    void run_after(duration_type duration, const Func& f) {
+        detail::make_timer_task(ios_, duration, f).push_task();
+    }
 };
 
 int g_val = 0;
@@ -79,6 +131,9 @@ void func_test() {
     if (g_val == 10) {
         throw boost::thread_interrupted();
     }
+
+    boost::this_thread::sleep_for(boost::chrono::milliseconds(100));
+
     if (g_val == 90) {
         tasks_processor::get().stop();
     }
@@ -100,8 +155,14 @@ int main(int argc, char *argv[])
 
     tasks_processor::get().push_task(boost::bind(std::plus<int>(), 2, 2));
 
+    std::cout << "Begin task" << std::endl;
+    tasks_processor::get().run_after(boost::posix_time::seconds(1), [] {
+        std::cout << "End task" << std::endl;
+    });
+
     assert(g_val == 0);
     tasks_processor::get().start();
     assert(g_val == 90);
+
     return 0;
 }
