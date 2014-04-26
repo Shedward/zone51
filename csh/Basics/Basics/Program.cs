60bd5ee020bd37e32e7c613d4ec15394ab7ea4db
Vlad Maltsev <shedwardx@gmail.com>
2014-04-27 01:10:41 +0400

    Basic usage of CSharp

---
+++ csh/Basics/Basics/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Basics
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2011, 10, 17);
+            Console.WriteLine("The day of {0} is {1}", dt.Date, dt.DayOfWeek);
+            var dt2 = dt.AddMonths(2);
+            Console.WriteLine("Daylight saving time: {0}", dt2.IsDaylightSavingTime());
+            Console.ReadKey();
+        }
+    }
+}
