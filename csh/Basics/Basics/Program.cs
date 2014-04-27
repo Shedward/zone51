3fe30dd32691b996b48b96c0fbbb977d0835b5c5
Vlad Maltsev <shedwardx@gmail.com>
2014-04-27 13:37:25 +0400

    Arithmetics, checked.

--- csh/Basics/Basics/Program.cs
+++ csh/Basics/Basics/Program.cs
@@ -9,7 +9,16 @@
             Console.WriteLine("The day of {0} is {1}", dt.Date, dt.DayOfWeek);
             var dt2 = dt.AddMonths(2);
             Console.WriteLine("Daylight saving time: {0}", dt2.IsDaylightSavingTime());
-            Console.ReadKey();
+
+            byte a = 100;
+            byte b = 212;
+            Console.WriteLine("100 + 212 = {0}", a + b);
+            Console.WriteLine("100 + 212 = {0}", (byte)(a + b));
+            checked
+            {
+                // Console.WriteLine("100 + 212 = {0}", (byte)(a + b));
+            }
+            // Console.WriteLine("100 + 212 = {0}", checked((byte)(a + b)));
         }
     }
 }
