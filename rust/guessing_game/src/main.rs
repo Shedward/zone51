4e6a770286c66b3442ed6bc03b6269dc48b7381b
Vlad Maltsev <shedwardx@gmail.com>
2015-09-02 18:52:21 +0500

    [Rust]

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -5,25 +5,26 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
-
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-
-    println!("The secret number is: {}", secret_number);
-
+    println!("Guess the number:");
     println!("Please input your guess.");
 
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("Guessed num: {}", secret_number);
 
+    let mut guess = String::new();	
     io::stdin().read_line(&mut guess)
-        .ok()
-        .expect("failed to read line");
+    	.ok()
+    	.expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse()
+    	.ok()
+    	.expect("Inputed string not a number");
 
     println!("You guessed: {}", guess);
 
     match guess.cmp(&secret_number) {
-        Ordering::Less    => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal   => println!("You win!"),
+    	Ordering::Less    => println!("Too small"),
+    	Ordering::Greater => println!("Too big"),
+    	Ordering::Equal   => println!("You win")
     }
 }
